6bcd34b0ad473b8bfaabba9fdd0797e02a40f296
Donald-Yang <donaldyang1028@gmail.com>
2023-11-04 15:47:54 -0400

    second time val

--- validity.py
+++ validity.py
@@ -1,3 +1,21 @@
-import openai 
+import os
+import openai
 
-def validate 
+def check_validity(text):
+    openai.api_key = os.getenv("OPENAI_API_KEY")
+
+    prompt = f"Fact check: {text}"
+    response = openai.Completion.create(
+        engine="gpt-3.5-turbo",
+        prompt=prompt,
+        max_tokens=50,  # Adjust the max tokens as needed to capture explanations
+    )
+
+    response_text = response.choices[0].text.strip()
+
+    if response_text == "False":
+        return {
+            "False": response.choices[1].text.strip() if len(response.choices) > 1 else "Explanation not provided"
+        }
+    else:
+        return True
