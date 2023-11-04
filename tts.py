d0cc214767f36244477994ca40d1ec59db40b0fc
Dominic <domlincog@gmail.com>
2023-11-04 17:40:24 -0400

    changed sound speed

--- tts.py
+++ tts.py
@@ -4,6 +4,7 @@ import pyttsx3
 engine = pyttsx3.init()
 
 def speak(text):
+    engine.setProperty('rate', 150) 
     engine.say(text)
     engine.runAndWait()
 
@@ -16,7 +17,7 @@ def main():
     # This is where you could read from a file if needed.
     # For example, if you have a file named 'message.txt' with the content "hello world", you could do:
     # message = read_file('message.txt')
-    
+       
     # But for now, we will just say "hello world" directly.
     speak("Hello world! If you can hear this the tts.py file is working properly!")
 
